0bb44374ec91d4d901811b144cd60de1dd8e90b6
TomChen <voyager.teng@gmail.com>
2024-10-31 16:23:58 +0800

    mallVoyager 初始化

---
+++ mallVoyager/src/main/java/com/voyager/mallVoyager/MallVoyagerApplication.java
@@ -0,0 +1,13 @@
+package com.voyager.mallVoyager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallVoyagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallVoyagerApplication.class, args);
+	}
+
+}
---
+++ mallVoyager/src/test/java/com/voyager/mallVoyager/MallVoyagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voyager.mallVoyager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallVoyagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
